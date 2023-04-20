65bf28f9ef317fadc19cf419ab65b13d8a3edd8a
simeunseo <rkdcjfehowl@naver.com>
2023-04-20 15:06:16 +0900

    add : sopt32 week2 과제2 추가

---
+++ sopt32/week2/assign2/common/todoData.js
@@ -0,0 +1,57 @@
+export const TODO_DATA = [
+  {
+    category: "과제",
+    list: [
+      {
+        content: "db 프로젝트",
+        done: true,
+      },
+      {
+        content: "pl 과제",
+        done: true,
+      },
+    ],
+  },
+  {
+    category: "공부",
+    list: [
+      {
+        content: "이산구조 복습",
+        done: false,
+      },
+      {
+        content: "react 강의",
+        done: true,
+      },
+    ],
+  },
+  {
+    category: "일과",
+    list: [
+      {
+        content: "일기 쓰기",
+        done: false,
+      },
+      {
+        content: "운동 1시간",
+        done: false,
+      },
+    ],
+  },
+  {
+    category: "동아리",
+    list: [
+      {
+        content: "솝트 스터디 신청",
+        done: true,
+      },
+    ],
+  },
+];
+
+export const CATEOGORY_COLOR = {
+  과제: "red",
+  공부: "green",
+  일과: "yellow",
+  동아리: "blue",
+};
